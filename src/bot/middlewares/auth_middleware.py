19431722de4a4d92fe5ea9a40473376744058c81
vas g <vas.g@me.com>
2024-05-26 19:20:52 +0400

    add ignoring admin as player

--- src/bot/middlewares/auth_middleware.py
+++ src/bot/middlewares/auth_middleware.py
@@ -7,6 +7,7 @@ from aiogram.types import Message
 import gspread
 
 from bot.internal.gsheets import create_new_player_sheet
+from config import settings
 from database.crud.user import add_user_to_db, get_user_from_db_by_tg_id
 
 
@@ -23,13 +24,14 @@ class AuthMiddleware(BaseMiddleware):
         if not user:
             user = await add_user_to_db(event.from_user, session)
             sheet_name = f'{user.fullname} bets'
-            with contextlib.suppress(gspread.exceptions.APIError):
-                # noinspection PyUnusedLocal
-                # add_new_sheet_task = asyncio.create_task(create_new_sheet(sheet_name, gspread_client))
-                # Update id=117922019 is handled. Duration 2623 ms by bot id=7063890126
-                add_new_sheet_task = asyncio.create_task(create_new_player_sheet(sheet_name, gspread_client))
-                # 26.05.2024|17:34:56+0400.168|INFO|dispatcher|feed_update: Update id=117922021 is handled. Duration 304 ms by bot id=7063890126
+            if event.from_user.id == settings.ADMIN:
+                with contextlib.suppress(gspread.exceptions.APIError):
+                    # noinspection PyUnusedLocal
+                    # add_new_sheet_task = asyncio.create_task(create_new_sheet(sheet_name, gspread_client))
+                    # Update id=117922019 is handled. Duration 2623 ms by bot id=7063890126
+                    add_new_sheet_task = asyncio.create_task(create_new_player_sheet(sheet_name, gspread_client))
+                    # 26.05.2024|17:34:56+0400.168|INFO|dispatcher|feed_update: Update id=117922021 is handled. Duration 304 ms by bot id=7063890126
 
-                # await asyncio.to_thread(await create_new_sheet(sheet_name, gspread_client))
+                    # await asyncio.to_thread(await create_new_sheet(sheet_name, gspread_client))
         data['user'] = user
         return await handler(event, data)
