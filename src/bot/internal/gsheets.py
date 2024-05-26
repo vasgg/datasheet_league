c5e22bf7405d01c0e0f7d86ca11a7c7ec259d855
vas g <vas.g@me.com>
2024-05-26 18:52:36 +0400

    google sheets stuff

--- src/bot/internal/gsheets.py
+++ src/bot/internal/gsheets.py
@@ -1,24 +1,32 @@
 import asyncio
-
+from concurrent.futures import ThreadPoolExecutor
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+
+from config import settings
 
 
-async def update_master_sheet(cell: str, value: int):
-    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-
-    credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
-    client = gspread.authorize(credentials)
-
-    spreadsheet = client.open("League_bets")
-    worksheet = spreadsheet.sheet1
-
-    await asyncio.to_thread(worksheet.update, cell, [[value]])
-    return {"message": f'Users counter updated. Cell: {cell} Value: {value}'}
+async def create_new_player_sheet(name: str, client: gspread.Client):
+    loop = asyncio.get_event_loop()
+    with ThreadPoolExecutor() as pool:
+        spreadsheet = await loop.run_in_executor(pool, client.open, settings.TABLE_NAME)
+        new_sheet = await loop.run_in_executor(pool, spreadsheet.add_worksheet, name, 100, 20)
+        header = ['Notes', 'Date sent', 'League', 'Bet Name', 'Risk', 'Odds', 'Win', 'Result', 'Net']
+        await loop.run_in_executor(pool, new_sheet.append_row, header)
 
 
-async def create_new_sheet(name: str, client: gspread.Client):
-    spreadsheet = client.open("League_bets")
-    new_sheet = spreadsheet.add_worksheet(title=name, rows=100, cols=20)
-    header = ['Notes', 'Date sent', 'League', 'Bet Name', 'Risk', 'Odds', 'Win', 'Result', 'Net']
-    new_sheet.append_row(header)
+async def post_to_master_sheet(data: dict, client: gspread.Client):
+    loop = asyncio.get_event_loop()
+    with ThreadPoolExecutor() as pool:
+        spreadsheet = await loop.run_in_executor(pool, client.open, settings.TABLE_NAME)
+        worksheet = await loop.run_in_executor(pool, spreadsheet.worksheet, "master")
+        row_values = [data.get(key, '') for key in data.keys()]
+        await loop.run_in_executor(pool, worksheet.append_row, row_values)
+
+
+async def post_to_player_sheet(sheet_name: str, data: dict, client: gspread.Client):
+    loop = asyncio.get_event_loop()
+    with ThreadPoolExecutor() as pool:
+        spreadsheet = await loop.run_in_executor(pool, client.open, settings.TABLE_NAME)
+        worksheet = await loop.run_in_executor(pool, spreadsheet.worksheet, sheet_name)
+        row_values = [data.get(key, '') for key in data.keys()]
+        await loop.run_in_executor(pool, worksheet.append_row, row_values)
