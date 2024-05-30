155c44a78c3020315b5ebe8ebc320a07ea1f0416
vas g <vas.g@me.com>
2024-05-30 18:20:57 +0400

    working on gsheets

--- src/bot/internal/gsheets.py
+++ src/bot/internal/gsheets.py
@@ -1,92 +1,80 @@
-import asyncio
-from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
 import logging
 
 import gspread
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from bot.internal.lists import MASTER_HEADER, MASTER_STATS, PLAYER_HEADER, PLAYER_STATS
 from config import settings
+from database.crud.event import count_events, get_average_weighted_odds_by_event_id, get_total_risk_by_event_id
+from database.models import Event
 
-MASTER_HEADER = ['Notes', 'ID', 'Date sent', 'League', 'Bet Name', 'Worst Odds', 'Responses']
-PLAYER_HEADER = ['Notes', 'ID', 'Date sent', 'League', 'Bet Name', 'Risk', 'Odds', 'Win', 'Result', 'Net']
-
-STATS_TABLE = [
-    ['average pos odds', '=AVERAGE(FILTER(G:G,G:G>0))'],
-    ['average neg odds', '=AVERAGE(FILTER(G:G,G:G<0))']
-]
 logger = logging.getLogger(__name__)
 
 
-async def add_stats_table_to_player_sheet(worksheet, pool):
-    loop = asyncio.get_event_loop()
-    await loop.run_in_executor(pool, worksheet.update_acell, 'K3', STATS_TABLE[0][0])
-    await loop.run_in_executor(pool, worksheet.update_acell, 'K5', STATS_TABLE[1][0])
-    await loop.run_in_executor(pool, worksheet.update_acell, 'K4', STATS_TABLE[0][1])
-    await loop.run_in_executor(pool, worksheet.update_acell, 'K6', STATS_TABLE[1][1])
-
-
-async def ensure_master_sheet(client: gspread.Client):
-    loop = asyncio.get_event_loop()
-    with ThreadPoolExecutor() as pool:
-        spreadsheet = await loop.run_in_executor(pool, client.open, settings.TABLE_NAME)
-        try:
-            worksheet = await loop.run_in_executor(pool, spreadsheet.worksheet, "master")
-        except gspread.exceptions.WorksheetNotFound:
-            logger.info("Creating 'master' sheet as it does not exist.")
-            worksheet = await loop.run_in_executor(pool, spreadsheet.add_worksheet, "master", 100, 20)
-            await loop.run_in_executor(pool, worksheet.append_row, MASTER_HEADER)
-            return
-
-        first_row = await loop.run_in_executor(pool, worksheet.row_values, 1)
-        if not first_row or first_row != MASTER_HEADER:
-            logger.info("Adding headers to 'master' sheet.")
-            await loop.run_in_executor(pool, worksheet.insert_row, MASTER_HEADER, 1)
+async def ensure_master_sheet(client: gspread.Client) -> gspread.Worksheet:
+    spreadsheet = client.open(settings.TABLE_NAME)
+    try:
+        worksheet = spreadsheet.worksheet("master")
+    except gspread.exceptions.WorksheetNotFound:
+        logger.info("Creating 'master' sheet as it does not exist.")
+        worksheet = spreadsheet.add_worksheet("master", 100, 20)
+        worksheet.append_row(MASTER_HEADER)
+    first_row = worksheet.row_values(1)
+    if not first_row or first_row != MASTER_HEADER:
+        logger.info("Adding headers to 'master' sheet.")
+        worksheet.insert_row(MASTER_HEADER)
+    worksheet.update([MASTER_STATS[0]], 'L3:R3')
+    worksheet.update([MASTER_STATS[1]], 'L4:R4', raw=False)
+    worksheet.update([MASTER_STATS[2]], 'L5:R5')
+    worksheet.update([MASTER_STATS[3]], 'L6:R6', raw=False)
+    return worksheet
 
 
 async def create_new_player_sheet(name: str, client: gspread.Client):
-    loop = asyncio.get_event_loop()
-    with ThreadPoolExecutor() as pool:
-        spreadsheet = await loop.run_in_executor(pool, client.open, settings.TABLE_NAME)
-        new_sheet = await loop.run_in_executor(pool, spreadsheet.add_worksheet, name, 100, 20)
-        await loop.run_in_executor(pool, new_sheet.append_row, PLAYER_HEADER)
-        await add_stats_table_to_player_sheet(new_sheet, pool)
+    spreadsheet = client.open(settings.TABLE_NAME)
+    new_sheet = spreadsheet.add_worksheet(name, 100, 20)
+    new_sheet.append_row(PLAYER_HEADER)
+    new_sheet.update([PLAYER_STATS[0]], 'K3:Q3')
+    new_sheet.update([PLAYER_STATS[1]], 'K4:Q4', raw=False)
+    new_sheet.update([PLAYER_STATS[2]], 'K5:Q5')
+    new_sheet.update([PLAYER_STATS[3]], 'K6:Q6', raw=False)
 
 
-async def post_to_master_sheet(data: dict, client: gspread.Client):
-    loop = asyncio.get_event_loop()
-    with ThreadPoolExecutor() as pool:
-        spreadsheet = await loop.run_in_executor(pool, client.open, settings.TABLE_NAME)
-        await ensure_master_sheet(client)
-        worksheet = await loop.run_in_executor(pool, spreadsheet.worksheet, "master")
-        row_values = [data.get(key, '') for key in data.keys()]
-        num_rows = len(await loop.run_in_executor(pool, worksheet.get_all_values))
-        await loop.run_in_executor(pool, worksheet.insert_row, row_values, num_rows + 1)
-        logger.info(f"Added row to 'master': {row_values}")
-
-        all_sheets = await loop.run_in_executor(pool, spreadsheet.worksheets)
-        sheet_names = [sheet.title for sheet in all_sheets if sheet.title != 'master']
-
-        countif_formula_parts = [f"COUNTIF('{sheet_name}'!B:B, B{num_rows + 1})" for sheet_name in sheet_names]
-        formula = f"= {' + '.join(countif_formula_parts)}"
-
-        await loop.run_in_executor(pool, worksheet.update_acell, f"G{num_rows + 1}", formula)
+async def post_to_master_sheet(event: Event, db_session: AsyncSession, client: gspread.Client):
+    line = await count_events(db_session) + 1
+    worksheet = await ensure_master_sheet(client)
+    bet = [[datetime.now().strftime("%-m/%-d/%Y"), event.league, event.bet_name, str(event.worst_odds)]]
+    win = [[f'=ROUND(IF(G{line}<0,(100/-G{line})*F2,(G{line}/100)*F{line}),0)']]
+    net = [[f'=IFERROR(IFS(REGEXMATCH(I{line}, "W"), H{line},REGEXMATCH(I{line}, "L"), 0-F{line}),0)']]
+    worksheet.update(bet, f'B{line}:E{line}')
+    worksheet.update(win, f'H{line}', raw=False)
+    worksheet.update(net, f'J{line}', raw=False)
+    logger.info(f"Added row to 'master': {bet}")
 
 
-async def get_column_height(worksheet, column_index, pool):
-    loop = asyncio.get_event_loop()
-    col_values = await loop.run_in_executor(pool, worksheet.col_values, column_index)
-    return len([value for value in col_values if value])
+async def post_to_player_sheet(sheet_name: str, data: list, line: int, client: gspread.Client):
+    spreadsheet = client.open(settings.TABLE_NAME)
+    worksheet = spreadsheet.worksheet(sheet_name)
+    more_data = [[
+        f'=ROUND(IF(F{line}<0,(100/-F{line})*E{line},(F{line}/100)*E{line}),0)',
+        f'=master!I{line}',
+        f'=IFERROR(IFS(REGEXMATCH(H{line}, "W"), G{line},REGEXMATCH(H{line}, "L"), 0-E{line}),0)'
+    ]]
+    worksheet.update(data, f'B{line}:F{line}')
+    worksheet.update(more_data, f'G{line}:I{line}', raw=False)
+    logger.info(f"Added row to '{sheet_name}': {data}")
 
 
-async def post_to_player_sheet(sheet_name: str, data: dict, client: gspread.Client):
-    loop = asyncio.get_event_loop()
-    with ThreadPoolExecutor() as pool:
-        spreadsheet = await loop.run_in_executor(pool, client.open, settings.TABLE_NAME)
-        worksheet = await loop.run_in_executor(pool, spreadsheet.worksheet, sheet_name)
-        row_values = [data.get(key, '') for key in PLAYER_HEADER]
-
-        num_rows = await get_column_height(worksheet, 2, pool)
-        await loop.run_in_executor(pool, worksheet.insert_row, row_values, num_rows + 1)
-
-        logger.info(f"Added row to '{sheet_name}': {row_values}")
+async def update_master_list_values(event_id: int, line: int, client: gspread.Client, db_session: AsyncSession):
+    total_risk = await get_total_risk_by_event_id(event_id, db_session)
+    average_odds = await get_average_weighted_odds_by_event_id(event_id, db_session)
+    spreadsheet = client.open(settings.TABLE_NAME)
+    worksheet = spreadsheet.worksheet("master")
+    worksheet.update([[total_risk, average_odds]], f'F{line}:G{line}')
 
 
+async def get_user_balance(fullname: str, client: gspread.Client) -> str:
+    spreadsheet = client.open(settings.TABLE_NAME)
+    worksheet = spreadsheet.worksheet(f"{fullname} bets")
+    return worksheet.acell('K4').value
